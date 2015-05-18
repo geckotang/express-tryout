9527dc4e118e216867013d33f979c6efdc5f92f1
geckotang <macky1207@gmail.com>
2015-05-18 20:32:28 +0900

    handlebarsを使用する準備をした

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,54 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var args = require('yargs').argv;
 var browserSync = require('browser-sync');
+var del = require('del');
+var plugin = require('gulp-load-plugins')();
 
 // settings
 var config = JSON.parse(fs.readFileSync(__dirname+'/config.json'));
 var BROWSER_SYNC_RELOAD_DELAY = 500;
 var env = args.env || 'development';
 var isProd = env === 'production';
+var srcDir = {
+  pc: './src/pc',
+  sp: './src/sp'
+};
+var destDir = {
+  pc: './www',
+  sp: './www/s'
+};
 
 gulp.task('default', ['browser-sync']);
+
+//PC用HTMLビルド
+gulp.task('html:pc', function() {
+  var templateData = {},
+  options = {
+    batch : [srcDir.pc + '/templates/partials'],
+    helpers : {}
+  };
+  return gulp.src(srcDir.pc + '/templates/pages/*.hbs')
+    .pipe(plugin.compileHandlebars(templateData, options))
+    .pipe(plugin.rename(function(path) {
+        path.extname = '.html';
+    }))
+    .pipe(gulp.dest(destDir.pc));
+});
+
+//SP用HTMLビルド
+gulp.task('html:sp', function() {
+  var templateData = {},
+  options = {
+    batch : [srcDir.sp + '/templates/partials'],
+    helpers : {}
+  };
+  return gulp.src(srcDir.sp + '/templates/pages/*.hbs')
+    .pipe(plugin.compileHandlebars(templateData, options))
+    .pipe(plugin.rename(function(path) {
+        path.extname = '.html';
+    }))
+    .pipe(gulp.dest(destDir.sp));
+});
 
 gulp.task('browser-sync', ['server'], function() {
 	browserSync.init(null, {
